9af9856d1c7af4f7be6e7471521d8b9bfa47a3af
syuu-22397 <66163698+syuu-22397@users.noreply.github.com>
2020-08-12 20:53:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,14 +20,26 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="#.help|仮運用中"))
+    evals.setup(bot)
+    print("logged in as " + bot.user.name)
+    for channel in bot.get_all_channels():
+        if channel.name == 'しゅーBot起動時メッセージ':
+            await channel.send('おはようございます！\n今日も頑張りましょう！')
 
+@bot.command()
+async def help(ctx):
+    await ctx.send('準備中です。')
+            
 @bot.command()
 async def cmd-test(ctx):
     await ctx.send('コマンドは受理されています。')
     
 @bot.command()
-async def (ctx):
-    await ctx.send('コマンドは受理されています。')    
+async def ペテルギウス(ctx):
+    await ctx.send('あなた、怠惰ですね?')    
 
 @client.event
 async def on_message(message):
@@ -46,5 +58,7 @@ async def pureityuudadada(ctx, *, st):
         await ctx.send(embed=discord.Embed(title="変更しました！", description=f"{st}"))
     else:
         await ctx.send(embed=discord.Embed(title="あなたは違いますよ！？", description="何しているんですか！？"))
+            
+ 
             
 bot.run(token)
